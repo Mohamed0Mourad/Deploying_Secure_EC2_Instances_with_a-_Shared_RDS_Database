b4ffa6e2ab9ccae7c000f816360105224e9e7ac3
Mohamed Mourad <mohamed0mourad@gmail.com>
2025-03-17 04:29:57 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA6ODU77JDSHHMJ5QS" #add your key
-  secret_key = "Ers/1oLYPlw6U0yQlg/qPPQaQ7PDQOyrD6dGPfso"
+  access_key = "AKIA6............" #add your key
+  secret_key = "Ers/1oLYPl............"
 }
