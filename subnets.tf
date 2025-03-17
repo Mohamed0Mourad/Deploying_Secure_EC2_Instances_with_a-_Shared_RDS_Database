91021c3d4f32dd23b1652a56755c04be22fcac13
Mohamed Mourad <mohamed0mourad@gmail.com>
2025-03-17 03:22:37 +0200

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -1,16 +1,18 @@
-resource "aws_subnet" "app_subnet_1" {
-  vpc_id            = aws_vpc.app_vpc.id
+resource "aws_subnet" "AppSubnet1" {
+  vpc_id            = aws_vpc.AppVPC.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+
   tags = {
     Name = "AppSubnet1"
   }
 }
 
-resource "aws_subnet" "app_subnet_2" {
-  vpc_id            = aws_vpc.app_vpc.id
+resource "aws_subnet" "AppSubnet2" {
+  vpc_id            = aws_vpc.AppVPC.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
+
   tags = {
     Name = "AppSubnet2"
   }
