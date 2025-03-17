b3712386026459b99e036875b3415d5e340b5c45
Mohamed Mourad <mohamed0mourad@gmail.com>
2025-03-17 03:33:54 +0200

    Update IGW.tf

--- IGW.tf
+++ IGW.tf
@@ -16,3 +16,11 @@ resource "aws_route_table" "AppRouteTable" {
 output "route_table_ID" {
   value = aws_route_table.AppRouteTable.id
 }
+
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.AppRouteTable.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.AppIGW.id
+}
+
